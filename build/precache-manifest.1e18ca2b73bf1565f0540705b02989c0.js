eeb52488d063248a5a1655da9805c6143d324568
boubamg <bouba.maiga99@gmail.com>
2020-11-06 11:19:58 +0100

    deploy

---
+++ build/precache-manifest.1e18ca2b73bf1565f0540705b02989c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb9eae2ad774cdf4f9a023b4e56042f7",
+    "url": "/P7_frontend/index.html"
+  },
+  {
+    "revision": "3627db831921d8f3ecff",
+    "url": "/P7_frontend/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3ad081d306dc3dabaeab",
+    "url": "/P7_frontend/static/css/main.6af1e7f7.chunk.css"
+  },
+  {
+    "revision": "3627db831921d8f3ecff",
+    "url": "/P7_frontend/static/js/2.ad5919f7.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/P7_frontend/static/js/2.ad5919f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ad081d306dc3dabaeab",
+    "url": "/P7_frontend/static/js/main.81a7c717.chunk.js"
+  },
+  {
+    "revision": "f0399c9dec96eb618460",
+    "url": "/P7_frontend/static/js/runtime-main.12bf956c.js"
+  }
+]);
